eb1ac91b542fd2a6fa1b89d96e74d6cc8aba3373
Sergey Srabionyan <prinzplanloser0514@gmail.com>
2021-03-29 21:33:47 +0300

    Добавление телеграм структур

---
+++ domain/telegram.go
@@ -0,0 +1,29 @@
+package domain
+
+type Update struct {
+	Id      int     `json:"update_id"`
+	Message Message `json:"message"`
+}
+
+type Message struct {
+	Chat Chat   `json:"chat"`
+	Text string `json:"text"`
+}
+
+type Chat struct {
+	ChatId int `json:"id"`
+}
+
+type UpdateResponse struct {
+	Result []Update `json:"result"`
+}
+
+type BotMessage struct {
+	ChatId int    `json:"chat_id"`
+	Text   string `json:"text"`
+}
+
+type Photo struct {
+	ChatId int    `json:"chat_id"`
+	Photo  string `json:"photo"`
+}
